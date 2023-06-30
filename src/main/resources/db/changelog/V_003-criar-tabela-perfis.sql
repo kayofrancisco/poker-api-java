f9ae8a24cc09c2cdc9dc5ee65ce6c8920239f828
Kayo Francisco Silva Bezerra <kayofrancisco@hotmail.com>
2023-06-30 20:15:32 -0300

    Revert "Merge branch 'feature/migrations' into develop"
    This reverts commit b877ba7f9ce2882354ad4e7d62d7207ee4ff33ca, reversing
    changes made to 3c54c735758418687fc3b4d7eab1c2ba5311c6fc.

--- src/main/resources/db/changelog/V_003-criar-tabela-perfis.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE perfis (
-	id serial4 NOT NULL,
-	descricao varchar NOT NULL,
-	sigla varchar NOT NULL,
-	CONSTRAINT perfis_pk PRIMARY KEY (id)
-);
--- src/main/resources/db/changelog/V_004-popular-tabela-perfis.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO perfis (descricao,sigla) VALUES
-	 ('Administrador','ADM'),
-	 ('Comum','COM'),
-	 ('Gestor Time','GET'),
-	 ('Aluno Time','ALT');
